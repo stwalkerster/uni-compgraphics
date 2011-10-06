7189c8990a34924ba0626bc4eb521fff2497f91b
Vasileos Spyridakis <s17billsp@me.com>
2011-10-06 10:08:45 +0100

    Entered push and popqcommands to the drawS.cpp and changed the SRT parameters

--- drawS.cpp
+++ drawS.cpp
@@ -5,6 +5,7 @@
 
 void drawS()
 {
+	glPushMatrix();
 	glTranslatef(0,1.3,0);
 	glScalef(0.75,0.75,0.75);
 	draw3Dcurve  (1.0,          //depth  
@@ -13,17 +14,19 @@ void drawS()
 				  0.0,          //start angle //0.0
 				  270,  		//stop angle
 				  5.0);         //anular increments
+				  glPopMatrix();
 				  
-				  
-				  
-	glTranslatef(0,-3.5,0);
+	glPushMatrix();			  
+	glTranslatef(0,-1.32,0);
 	glRotatef(180,0,0,1);
+	glScalef(0.75,0.75,0.75);
 	draw3Dcurve  (1.0,          //depth  
 				  1.5,          //inner radius
 				  2.0,          //outer radius
 				  0.0,          //start angle //0.0
 				  270,  		//stop angle
 				  5.0);
+				  glPopMatrix();
 				  //glTranslatef(0,1.75,0);
 				  //glRotatef(45,1,0,0);
 				  //glScalef(0.65,0.65,0.65);
