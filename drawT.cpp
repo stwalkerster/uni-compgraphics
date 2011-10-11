eecadd5d5b3aa10c3af4b530b6b58a94b046f71e
Simon Walker <github@stwalkerster.co.uk>
2011-10-11 14:26:57 +0100

    added initial work on T

--- drawT.cpp
+++ drawT.cpp
@@ -5,15 +5,34 @@
 
 void drawT()
 {
-
 	// draw stem of t
+	glPushMatrix();
+		glTranslatef(0,-1,0);
+		//r
+		glScalef(1,3,1);
+		cube();
+	glPopMatrix();
+
 
 	// draw curve of t
+	glPushMatrix();
+		//
+	glPopMatrix();
+
 
 	// draw left cross of t
+	glPushMatrix();
+		glTranslatef(-1,0,0);
+		//r
+		//s
+		cube();
+	glPopMatrix();
 
 	// draw right cross of t
-
-	
-
+	glPushMatrix();
+		glTranslatef(1,0,0);
+		//r
+		//s
+		cube();
+	glPopMatrix();
 }
