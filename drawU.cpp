0813095563a0c0d24a3b9a78f6b48a3f5c9e8b05
Simon Walker <github@stwalkerster.co.uk>
2011-10-21 15:06:07 +0100

    first attempt - implement U

--- drawU.cpp
+++ drawU.cpp
@@ -1,9 +1,30 @@
 #include <GL/glut.h>
 #include "drawU.h"
-#include "3DCurve.h"
-#include "Cube.h"
+#include "drawW.h"
 
 void drawU()
 {
+	// draw left |
+	glPushMatrix();
+		glTranslatef(-1.5,1,0);
+		//r
+		//s
+		drawStick();
+	glPopMatrix();
 
+	// draw right |
+	glPushMatrix();
+		glTranslatef(0,0.5,0);
+		//r
+		glScalef(1,1.5,1);
+		drawStick();
+	glPopMatrix();
+
+	// draw U
+	glPushMatrix();
+		glTranslatef(-0.75,0,0);
+		//r
+		//s
+		drawLoop();
+	glPopMatrix();
 }
