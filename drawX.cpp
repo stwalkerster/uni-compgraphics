277a65ca86f54bf67ba0cca6e2fff07a0e118b2d
Vasileios Spyridakis <s17billsp@Bill-Netbook.(none)>
2011-10-28 11:40:12 +0100

    Finished drawX.cpp

--- drawX.cpp
+++ drawX.cpp
@@ -8,19 +8,31 @@ void drawX()
 	glPushMatrix();
 		glTranslatef(0,0,0);
 		//glRotatef(45,0,0,1);
-		glScalef(0.3,0.3,0.3);
+		glScalef(0.5,0.5,0.5);
 		cube();
 	glPopMatrix();
 	
 	glPushMatrix();
-		//glTranslatef(0,1.3,0);
-		glRotatef(45,0,0,1);
+		glTranslatef(1.35,0,0);
+		glRotatef(95,0,0,1);
 		glScalef(0.75,0.75,0.5);
 		draw3Dcurve  (1.0,          //depth  
 					  1.5,      //inner radius
 					  2.0,      //outer radius
 					  0.0,      //start angle
-					  150,      //stop angle
+					  170,      //stop angle
+					  5.0);     //anular increments
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(-1.35,0,0);
+		glRotatef(-85,0,0,1);
+		glScalef(0.75,0.75,0.5);
+		draw3Dcurve  (1.0,          //depth  
+					  1.5,      //inner radius
+					  2.0,      //outer radius
+					  0.0,      //start angle
+					  170,      //stop angle
 					  5.0);     //anular increments
 	glPopMatrix();
 
