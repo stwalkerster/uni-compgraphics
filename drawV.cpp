2749b0cafcf1da9c7551400a6430474e481d76d4
Vasileos Spyridakis <s17billsp@me.com>
2011-11-02 11:56:01 +0000

    removed commented code

--- drawV.cpp
+++ drawV.cpp
@@ -7,7 +7,7 @@ void drawV()
 {
 	glPushMatrix();
 		glTranslatef(-1.05,0,0);
-		glRotatef(15,0,0,1);
+		glRotatef(15,0,0,1);//
 		glScalef(0.4,4,0.5);
 		cube();
 	glPopMatrix();
@@ -28,18 +28,5 @@ void drawV()
 		glRotatef(-15,0,0,1);
 		glScalef(0.4,4,0.5);
 		cube();
-	glPopMatrix();			  
-	/*			  
-	glTranslatef(0,-3.5,0);
-	glRotatef(180,0,0,1);
-	draw3Dcurve  (1.0,          //depth  
-				  1.5,          //inner radius
-				  2.0,          //outer radius
-				  0.0,          //start angle //0.0
-				  270,  		//stop angle
-				  5.0);
-				  //glTranslatef(0,1.75,0);
-				  //glRotatef(45,1,0,0);
-				  //glScalef(0.65,0.65,0.65);
-				  //cube();*/
+	glPopMatrix();		  
 }
