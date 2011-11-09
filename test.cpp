070e40b4305fa00378b414cecaf68f6e51f851a5
Vasileos Spyridakis <s17billsp@me.com>
2011-11-09 11:47:23 +0000

    created the pig's foot.

--- test.cpp
+++ test.cpp
@@ -4,8 +4,39 @@
 
 void test()
 {
+	glTranslatef(0,-1.25,0);
 	glPushMatrix();
-		//glTranslatef();
+		glRotatef(90,1,0,0);
+		Ucube();
+	glPopMatrix();
+	
+	glPushMatrix();
+		glTranslatef(0,0.5,0);
+		glRotatef(90,1,0,0);
+		Ucube();
+	glPopMatrix();
+	
+	glPushMatrix();
+		glTranslatef(0,1,0);
+		glRotatef(90,1,0,0);
+		Ucube();
+	glPopMatrix();
+	
+	glPushMatrix();
+		glTranslatef(0,1.5,0);
+		glRotatef(90,1,0,0);
+		Ucube();
+	glPopMatrix();	
+	
+	glPushMatrix();
+		glTranslatef(0,2,0);
+		glRotatef(90,1,0,0);
+		Ucube();
+	glPopMatrix();	
+	
+	glPushMatrix();
+		glTranslatef(0,2.5,0);
+		glRotatef(90,1,0,0);
 		Ucube();
 	glPopMatrix();	
 }
