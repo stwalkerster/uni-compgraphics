3bf72162f8dc819fa8fcc1e7f44dd377007d4dc6
Simon Walker <github@stwalkerster.co.uk>
2011-11-16 17:39:25 +0000

    nose, needs repositioning

--- drawNose.cpp
+++ drawNose.cpp
@@ -3,7 +3,18 @@
 
 void drawNose()
 {glPushMatrix();
+	glTranslatef(0,-0.5,-0.25);
+	glRotatef(-90,0,1,0);
 	
-
+	drawU();
+	
+	int steps = 24;
+	for(int i = 0; i<steps;i++)
+	{
+		glPushMatrix();
+			glRotatef((360/steps) * i, 1,0,0);
+			drawU();
+		glPopMatrix();
+	}
 
 glPopMatrix();}
