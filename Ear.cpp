bf21d2fb751a91cc4ca181634e3a562a489766b5
Simon Walker <github@stwalkerster.co.uk>
2011-11-16 10:35:42 +0000

    done the ear

--- Ear.cpp
+++ Ear.cpp
@@ -2,22 +2,31 @@
 #include "drawV.h"
 
 void drawEar()
-{
+{glPushMatrix();
+	
+	glTranslatef(0,1.8,0);
+	glRotatef(180,1,0,0);
+	
 	glPushMatrix();
 		glTranslatef(0,0.93,0);
-		glRotatef(180,1,0,0);
+		//r
 		glScalef(0.5,0.5, 0.5);
 		drawV();
 	glPopMatrix();
 
-	glPushMatrix();
-		glTranslatef(0,0.85,0.09);
-		glRotatef(180,1,0,0);
-		glScalef(0.4,0.4, 0.25);
-		drawV();
-	glPopMatrix();
+	float inc = 0.28;
+	for(int i = 0; i < 8; i++)
+	{
+		glPushMatrix();
+			glTranslatef(0, 0.85 + (i*inc), 0.09);
+			//r
+			glScalef(0.4,0.4, 0.25);
+			drawV();
+		glPopMatrix();
+	}
 
 
-}
+
+glPopMatrix();}
 
 
